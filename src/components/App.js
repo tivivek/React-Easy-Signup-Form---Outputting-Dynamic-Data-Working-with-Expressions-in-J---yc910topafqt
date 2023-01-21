2264e24a15b49c3abba38398978f2a226f652cfa
Vivek Tiwari <79350756+tivivek@users.noreply.github.com>
2023-01-21 23:38:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,190 +1,56 @@
-import React, {Component, useState} from "react";
+import React, { useState } from "react";
 
-import '../styles/App.css';
-
-
- 
-
+import { signUpFormValidation } from "./../utils/validation";
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  consent: "off"
+};
 const App = () => {
-
-  let genders = ['female', 'male', 'other']
-
-  const [errorMessage, setError] = useState('')
-
-  const [name, setName] = useState('')
-
-  const [email, setEmail] = useState('')
-
-  const [gender, setGender] = useState('male')
-
-  const [phone, setPhone] = useState('')
-
-  const [password, setPassword] = useState('')
-
-  const handleName = (e) => {
-
-      setName(e.target.value)
-
-  }
-
-  const handleEmail = (e) => {
-
-      setEmail(e.target.value)
-
-  }
-
-  const handlePhone = (e) => {
-
-    setPhone(e.target.value)
-
-  }
-
-  const handlePassword = (e) => {
-
-     setPassword(e.target.value)
-
-  }
-
-  const handleClick = (e) => {
-
-      e.preventDefault();
-
-      if(name === '' || email === '' || phone === '' || password === '') {
-
-         setError('All fields are mandatory')
-
-         return
-
-      }
-
-        var regEx = /^[a-z0-9' ']+$/i;
-
-        if(name.charAt(0) === ' ' ) {
-
-          setError('Name is not alphanumeric')
-
-          return
-
-        }
-
-        if(!regEx.test(name)) {
-
-            setError('Name is not alphanumeric')
-
-            return
-
-        }
-
-   
-
-        if(!email.includes('@')) {
-
-            setError('Email must contain @')
-
-            return
-
-        }
-
-       
-
-        if(isNaN(phone)) {
-
-          setError('Phone Number must contain only numbers')
-
-          return
-
-        }
-
-        if(password.length < 6) {
-
-          setError('Password must contain atleast 6 letters')
-
-          return
-
-        }
-
-      else {
-
-        let info = email;
-
-        info = info.slice(0, info.indexOf("@"));
-
-        console.log(info);
-
-        setError("Hello " + info);
-
-      }
-
-  }
-
+  const [user, setUser] = useState(initialState);
+  const [result, setResult] = useState({});
+  const consentHandler = (e) => {
+    setUser({ ...user, [e.target.id]: e.target.checked });
+  };
+  const changeHandler = (e) => {
+    setUser({ ...user, [e.target.id]: e.target.value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setResult(signUpFormValidation(user));
+    console.log(result);
+  };
   return (
-
-    <div id="main">
-
-       <form>
-
-         <p>{errorMessage}</p>
-
-          <label>Name : </label>
-
-          <input data-testid = 'name' type='text' onChange={(e) => handleName(e)}></input>
-
-          <br/><br/>
-
-          <label>Email : </label>
-
-          <input data-testid = 'email' type='email' onChange={(e) => handleEmail(e)}></input>
-
-          <br/>
-
-          <br/>
-
-          <label>Gender : </label>
-
-          <select data-testid = 'gender' value={gender} onChange={(e) => setGender(e.target.value)}>
-
-           {genders.map((gender) => (
-
-              <option key={gender} value={gender}>{gender}</option>
-
-           )
-
-           )}
-
-          </select>
-
-          <br/>
-
-          <br/>
-
-          <label>Phone No. : </label>
-
-          <input data-testid = 'phoneNumber' type='phone' onChange={(e) => handlePhone(e)}></input>
-
-          <br/>
-
-          <br/>
-
-          <label>Password : </label>
-
-          <input data-testid = 'password' type='password' onChange={(e) => handlePassword(e)}></input>
-
-          <br/>
-
-          <br/>
-
-          <button  data-testid = 'submit' onClick={(e) => handleClick(e)}>Submit</button>
-
-       </form>
-
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="name">Name :</label>
+          <input type="text" id="name" onChange={(e) => changeHandler(e)} />
+          {result && <p style={{ color: "red" }}>{result.name}</p>}
+        </div>
+        <div>
+          <label htmlFor="email">Email :</label>
+          <input type="email" id="email" onChange={(e) => changeHandler(e)} />
+          {result && <p style={{ color: "red" }}>{result.email}</p>}
+        </div>
+        <div>
+          <label htmlFor="password">Password :</label>
+          <input type="text" id="password" onChange={(e) => changeHandler(e)} />
+          {result && <p style={{ color: "red" }}>{result.password}</p>}
+        </div>
+        <div>
+          <label htmlFor="consent">Consent :</label>
+          <input
+            type="checkbox"
+            id="consent"
+            onChange={(e) => consentHandler(e)}
+          />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
     </div>
-
-  )
-
-}
-
-
-
- 
+  );
+};
 
 export default App;
